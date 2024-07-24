9d1cb89bf1ece84f92e3a89d561447774edf87e8
Vishal Maurya <vishal.mourya@navgrahaa.com>
2024-07-24 19:07:55 +0530

    test for auto trigeers

--- index.js
+++ index.js
@@ -1,54 +1,70 @@
-const express = require('express');
-const socketio = require('socket.io');
-const http = require('http');
-const cors = require('cors');
-const moment = require('moment'); 
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const cors = require("cors");
+const moment = require("moment");
 
-const {addUser, removeUser, getUser, getUsersInRoom,} = require('./user');  
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./user");
 
 const PORT = process.env.PORT || 3001;
 
-
-const router = require('./router');
+const router = require("./router");
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
- 
-io.on('connection', (socket) => {
 
-    socket.on('join', ({ name, room }, callback) => {
-        const { error, user } = addUser({ id: socket.id, name, room });
-    
-        if(error) return callback(error);
-    
-        socket.join(user.room);
-    
-        socket.emit('message', { user: 'Sattvaconnect', text: `${user.name}, welcome to room ${user.room}.`});
-        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
-    
-        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
-    
-        callback();
-      });
-      
-      socket.on('sendMessage', (message, callback) => {
-        const user = getUser(socket.id);
-        let currentTime = moment().format();
-        io.to(user.room).emit('message', { user: user.name, text: message, time: currentTime });
-    
-        callback();
-      });
-    
-      socket.on('disconnect', () => {
-        const user = removeUser(socket.id);
-    
-        if(user) {
-          io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
-          io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
-        }
-      });
- });
+io.on("connection", (socket) => {
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
 
+    if (error) return callback(error);
+
+    socket.join(user.room);
+
+    socket.emit("message", {
+      user: "Sattvaconnect",
+      text: `${user.name}, welcome to room ${user.room}.`,
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name} has joined!` });
+
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+
+    callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+    let currentTime = moment().format();
+    io.to(user.room).emit("message", {
+      user: user.name,
+      text: message,
+      time: currentTime,
+    });
+
+    callback();
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User had left!!!");
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit("message", {
+        user: "Admin",
+        text: `${user.name} has left.`,
+      });
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
+    }
+  });
+});
 
 app.use(router);
 app.use(cors());
